af5d22039e17f03fdeea20ce113446b4daebab3a
Kindaglia <matteodrago007@gmail.com>
2022-05-19 11:48:10 +0200

    update date js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function myFunction(){
+  /*
     const word1 = {
         letter1: document.getElementById("letter1").value,
         color1: document.querySelector('input[name="color1"]:checked').value,
@@ -11,6 +12,35 @@ function myFunction(){
         letter5: document.getElementById("letter5").value,
         color5: document.querySelector('input[name="color5"]:checked').value,
     };
+*/
+
+
+    let word1 = [
+      {
+        letter: document.getElementById("letter1").value,
+        color: document.querySelector('input[name="color1"]:checked').value
+      },
+      {
+        
+        letter: document.getElementById("letter2").value,
+        color: document.querySelector('input[name="color2"]:checked').value
+      },
+      {
+        letter: document.getElementById("letter3").value,
+        color: document.querySelector('input[name="color3"]:checked').value
+        
+      },
+      {
+        letter: document.getElementById("letter4").value,
+        color: document.querySelector('input[name="color4"]:checked').value
+      },
+      {
+        letter: document.getElementById("letter5").value,
+        color: document.querySelector('input[name="color5"]:checked').value
+      }
+    ]
+
+
 
     //in console arriva un oggetto 
     console.log(word1);
